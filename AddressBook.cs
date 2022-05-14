2d6043b641ae365e0b4b6906f6adcf10b944211a
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-14 14:53:43 +0530

    [Zeeshan] : Added program to create a basic contact book

---
+++ AddressBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        //instance variables 
+        public string firstName;
+        public string lastName;
+        public string Address;
+        public string city;
+        public string state;
+        public int zip;
+        public long phoneNumber;
+        public string email;
+        //int contact = 0;
+
+
+        //Parameterised Constructor to setup contacts
+        public AddressBook(string firstName, string lastName, string Address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.Address = Address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+
+        }
+
+        public void displayContact()
+        {
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Name : {0} {1}",firstName, lastName);
+            Console.WriteLine("Address: "+Address);
+            Console.WriteLine("City : "+ city);
+            Console.WriteLine("State : " + state);
+            Console.WriteLine("ZipCode : " + zip);
+            Console.WriteLine("Phone Number : " + phoneNumber);
+            Console.WriteLine("Email ID : " + email );
+            Console.WriteLine("---------------------------------------------------------");
+
+        }
+
+    }
+
+    
+    
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace AddressBookSystem
             Console.WriteLine("");
             Console.WriteLine("\t\t\t\t\t Welcome to Address Book Program");
             Console.WriteLine("");
+            AddressBook contact1 = new AddressBook("Zeeshan","Sarwar","Nyati Euphoria","Viman Nagar","Pune",411011,9792432125,"z@gmail.com");
+            AddressBook contact2 = new AddressBook("John", "Doe", "Empire State Building", "NY", "New York", 21010, 5443567, "jdoe@gmail.com");
+            contact1.displayContact();
+            contact2.displayContact();
             Console.ReadKey();
         }
     }

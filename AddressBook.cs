41896fc0e267b1d18a904384c30aff7ae51fbb7a
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-15 19:44:24 +0530

    [Zeeshan] : Added comments for array objects

--- AddressBook.cs
+++ AddressBook.cs
@@ -17,7 +17,7 @@ namespace AddressBookSystem
         public int zip;
         public long phoneNumber;
         public string email;
-        public AddressBook[] ContactArray;
+        public AddressBook[] ContactArray;  //delclaration of an array to store class instances
         int contact = 0;
 
 
@@ -35,7 +35,7 @@ namespace AddressBookSystem
 
         }
 
-        //Default Contructor
+        //Default Constructor for array declaration
         public AddressBook()
         {
             this.ContactArray = new AddressBook[10];
@@ -51,10 +51,78 @@ namespace AddressBookSystem
             obj.Display(ContactArray, contact);
         }
 
+        public void Modify()
+        {
+            //User enters field to Modify
+            int i = 0;
+            Console.WriteLine("-------To Modify-------\nEnter first name of user that needs modification");
+            string name = Console.ReadLine();
+
+            //Traverse till the desired index
+            while (ContactArray[i].firstName != name)
+            {
+                i++;
+            }
+
+            Console.WriteLine("Enter field to be modified 1.firstName 2.lastName 3.Address 4.city 5.state 6.zip 7.phoneNumber 8.email 9.Delete a contact");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("Enter the modified value");
+                    string fn = Console.ReadLine();
+                    ContactArray[i].firstName = fn;
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the modified value");
+                    string ls = Console.ReadLine();
+                    ContactArray[i].lastName = ls;
+                    break;
+                case 3:
+                    Console.WriteLine("Ente the modified value");
+                    string add = Console.ReadLine();
+                    ContactArray[i].Address = add;
+                    break;
+                case 4:
+                    Console.WriteLine("Enter the modified value");
+                    string cities = Console.ReadLine();
+                    ContactArray[i].city = cities;
+                    break;
+                case 5:
+                    Console.WriteLine("Enter the modified value");
+                    string states = Console.ReadLine();
+                    ContactArray[i].state = states;
+                    break;
+                case 6:
+                    Console.WriteLine("Enter the modified value");
+                    int temp = Convert.ToInt32(Console.ReadLine());
+                    ContactArray[i].zip = temp;
+                    break;
+                case 7:
+                    Console.WriteLine("Ente the modified value");
+                    int phn = Convert.ToInt32(Console.ReadLine());
+                    ContactArray[i].phoneNumber = phn;
+                    break;
+                case 8:
+                    Console.WriteLine("Ente the modified value");
+                    string emails = Console.ReadLine();
+                    ContactArray[i].email = emails;
+                    break;
+                //Delete a user
+                case 9:
+                    ContactArray = ContactArray.Take(i).Concat(ContactArray.Skip(i + 1)).ToArray();
+                    contact--;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+            //Display Function
+            AddBooks obj = new AddBooks();
+            obj.Display(ContactArray, contact);
+
+        }
+
     }
 
-    
-    
-
-    
 }
